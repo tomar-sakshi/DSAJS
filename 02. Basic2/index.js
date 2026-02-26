d57a46dfe92bb06ff08f97f1f8618a605f44cdf9
ch-sakshi <ch.sakshi005@gmail.com>
2026-02-26 23:31:07 +0530

    add: function

---
+++ 02. Basic2/index.js
@@ -0,0 +1,97 @@
+// function printHelloWorld(){
+//     console.log("Hello World");
+// }
+//  let a = 10;
+
+//  printHelloWorld();
+
+// function greet(name){
+//     console.log("Namaste, "  + name);
+
+// }
+
+// let x = "Virat Kohli"
+// greet(x);//Namaste, Virat Kohli
+
+// Create a Function that can add two numbers.......
+
+// function sum(a, b) {
+//   let add = a + b;
+//   console.log(add);
+// }
+
+// let x = 18;
+// let y = 23;
+// let z = 11;
+
+// sum(x, z);
+// sum(x, y);
+
+// Multiply
+// function multiply(a, b) {
+//   let multiply = a * b;
+//   console.log(multiply);
+// }
+
+// let a = 8;
+// let b = 23;
+
+// multiply(a, b);
+
+// create a square function.....
+
+// function square(x) {
+//     let result = x * x;
+//     return result;
+// }
+
+// let value = square (-3);
+
+// console.log(value);
+
+// let z = square(7);
+
+// console.log(z);
+
+// Create a function which accepts the age and tells whether a person is eligible to vote or not.......
+
+// function eligibileToVote(age) {
+//     if(age < 18) {
+//         console.log("Not Eligibile to Vote");
+//     }
+//     else {
+//         console.log("Eligibile to Vote")
+//     }
+// }
+// eligibileToVote(18);
+
+// function eligibileToVote(age) {
+//   if (age < 0) {
+//     console.log("Invaild Input");
+//   } else if(age<18){
+//     console.log("Not Eligible");
+//   } else {
+//     console.log("Eligible");
+//   }
+//   }
+
+// eligibileToVote(8);//Not Eligible
+// eligibileToVote(18);//Eligible
+// eligibileToVote(82);//Eligible
+
+// Create a function to check if a number is Even and odd.....
+
+// function isEvenOdd (num) {
+//     let rem = num % 2;
+
+//     if(rem == 0){
+//         console.log("Even Number")
+//     }
+//     else {
+//         console.log("Odd Number")
+//     }
+// }
+
+// isEvenOdd(6);
+// isEvenOdd(55);
+// isEvenOdd(65);
