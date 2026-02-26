7584a128a0cebfed851b860afa46fd4dd726df1b
ch-sakshi <ch.sakshi005@gmail.com>
2026-02-26 23:30:37 +0530

    add: variables

---
+++ 01. Basic1/index.js
@@ -0,0 +1,73 @@
+// console.log("Hello World");
+// console.log("Sakshi Tomar");
+// console.log("Sakshi 2");
+// console.log("Sakshi 3");
+
+// How to store value in variables .......2 numbers (+,-,*)
+
+// const a = 20;
+// const b = 30;
+
+// const sum = a + b;
+// const difference = a - b;
+// const multiply = a * b;
+
+// console.log(sum,difference,multiply);
+
+// Const and let
+
+// const a = 10; //in case of const we cannot change the value of variable it shows errors.
+
+// console.log(a); 
+
+// let b = 20 ;// in case of let we can change the value of variable.
+
+//  b = 40;
+
+// console.log(b);
+
+//  Create variables in Strings...
+// let firstName = "Sakshi";
+// let lastName = "Tomar";
+
+// firstName = "Suryanshu";
+
+// let fullName = firstName +" "+ lastName;// if we want space between in name we have to add empty strings.
+
+// console.log(fullName);//Suryanshu Tomar
+
+// Create Array
+
+// let arr = [10,20,34,4,65,0,66];
+
+// store the arr behind the scenes.............
+// index = 0   1   2  3   4  5  6  
+// arr   = 10  20  34 4   65 0  66  
+//  let sum = arr[1] + arr[5];
+
+// console.log(sum);//20
+// console.log(arr[7]); //undefined
+
+// Create arr in strings......
+
+// let arr = ["Sakshi", "Suryanshu", "Khushi", "Mummy", "Ujjwal"];
+
+// console.log(arr[2]);
+
+// let arr = ["Sakshi", "Suryanshu", 7, [2, 4, [30, 20 ]],10 , false, {name: "Khushi"}];
+
+
+// console.log(arr[3][2][1]);
+// console.log(arr[6].name);
+
+// Create arr in object
+
+// let obj = {
+//     a: 7,
+//     firstName: "Sakshi",
+//     lastName: "Tomar",
+//     bool: true,
+//     arr: [6,8,9]
+// }
+
+// console.log(obj.firstName + obj.lastName);
