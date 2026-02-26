35b493e7721ebac1b3240cbb316d49a90c99da6f
ch-sakshi <ch.sakshi005@gmail.com>
2026-02-26 23:31:26 +0530

    add: loops

---
+++ 03. Loops/index.js
@@ -0,0 +1,19 @@
+// for loops
+
+// for(let i = 3; i < 5 ;i++){
+//     console.log("Hello World " +  i)
+// }
+
+// for (let i = 2; i < 9; i = i+2){
+//     console.log("Hello Baby")
+// }
+
+// for (let i = 5; i > 0; i = i-1){
+//     console.log("Hello Baby " + i)
+// }
+
+// for (let i = 5; i < 4; i++){
+//     console.log ("Hello World " + i)
+// }
+
+// console.log("check");
